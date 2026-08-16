6f7e6d7522010a35b1201fc36b1ef85659a30d41
randomboi404 <tanishsawant603@gmail.com>
2026-08-16 17:49:27 +0530

    Disable using bt headphone mic

--- modules/aspects/system/audio.nix
+++ modules/aspects/system/audio.nix
@@ -1,17 +1,21 @@
 {
   system.audio = {
-    nixos =
-      { ... }:
-      {
-        security.rtkit.enable = true;
+    nixos = {
+      security.rtkit.enable = true;
 
-        services.pipewire = {
-          enable = true;
-          alsa.enable = true;
-          alsa.support32Bit = true;
-          pulse.enable = true;
-          jack.enable = true;
+      services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        jack.enable = true;
+
+        wireplumber.extraConfig."10-no-autoswitch" = {
+          "wireplumber.settings" = {
+            "bluetooth.autoswitch-to-headset-profile" = false;
+          };
         };
       };
+    };
   };
 }
